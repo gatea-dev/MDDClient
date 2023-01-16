6ea811d77da7909b60111224d9fb76e9de32ad51
John Shaffer <johns@gatea.com>
2023-01-16 09:10:10 -0500

    SplineMakerCLI

--- linux64/install.bash
+++ linux64/install.bash
@@ -6,8 +6,8 @@ GIT_HOME=`pwd`
 
 ## MDD Tools
 
-for EXE in `ls bin64`
+for EXE in `ls bin64/*Debug*`
 do
    echo "Installing ${EXE}"
-   sudo cp -p ${GIT_HOME}/bin64/${EXE}* /usr/local/bin/MDDTools/bin64
+   sudo cp -p ${GIT_HOME}/${EXE}* /usr/local/bin/MDDTools/bin64
 done
